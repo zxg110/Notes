7f658a31c8ccc84c701a161cc4e719b450d350d5
zxg <493155127@qq.com>
2016-01-08 11:24:52 +0800

    update at 2016-01-08

--- src/com/zxg/notes/activity/NotesMainActivity.java
+++ src/com/zxg/notes/activity/NotesMainActivity.java
@@ -12,12 +12,10 @@ import android.os.Bundle;
 import android.app.Activity;
 
 import android.content.Intent;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
-import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
